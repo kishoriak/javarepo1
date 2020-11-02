7cd224feb41361aad2b4a683ed18d7e328b236a8
kishori <kishoritrg@gmail.com>
2020-11-02 17:44:20 +0530

    day 6 deomos

--- MyCrudArrayList/src/com/demo/service/ProductService.java
+++ MyCrudArrayList/src/com/demo/service/ProductService.java
@@ -3,13 +3,24 @@ package com.demo.service;
 import java.util.List;
 
 import com.demo.bean.Product;
+import com.demo.exception.ProductNotFoundException;
 
 public interface ProductService {
 
 	void addProduct();
 
 	List<Product> displayAll();
-
+    //search product by id
 	Product searchProduct(int id);
+
+	boolean modifyName(int id, String nm);
+
+	boolean modifyNameandQty(int id, String nm, int qty);
+
+	Product searchByName(String nm) throws ProductNotFoundException;
+
+	List<Product> sortData();
+
+	//void addProduct(int id, String nm, int qty, String desc);
 
 }
